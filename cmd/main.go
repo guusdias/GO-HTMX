8dc128d771a4945082db281d29e0193efe7d6cbc
guusdias <gustavohd34@gmail.com>
2025-04-14 23:44:14 -0300

    FIRST COMMIT

---
+++ cmd/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+type Count struct {
+	Count int
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Template {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+	return t
+}
+
+func main() {
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	count := Count{Count: 0}
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		count.Count++
+		return c.Render(http.StatusOK, "index", count)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
